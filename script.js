7bee832e5602162bd2ed70aa5311dbf5ab5636d5
FameZeeshan <zeeshan6143@gmail.com>
2022-11-13 22:39:37 +0530

    final RPS

--- script.js
+++ script.js
@@ -2,64 +2,70 @@
 // elements
 const userScoreEl = document.getElementById("user-score");
 const compScoreEl = document.getElementById("comp-score");
+
 const userChoiceEl = document.getElementById("user-choice");
 const compChoiceEl = document.getElementById("comp-choice");
+const resultEl = document.getElementById("result");
 // global variables
-let userChoice = "";
-let computerChoice = "";
+let userScore = 0;
+let compScore = 0;
 
 // functions
-const getComputerChoice = function () {
+const getCompChoice = function () {
   const weapons = ["rock", "paper", "scissors"];
   const randomNumber = Math.trunc(Math.random() * 3);
   const randomChoice = weapons[randomNumber];
   return randomChoice;
 };
 
-const getResult = function (userChoice, computerChoice) {
+const getResult = function (userChoice, compChoice) {
   if (userChoice === "rock") {
-    if (computerChoice === "rock") {
+    if (compChoice === "rock") {
       return "Draw";
-    } else if (computerChoice === "paper") {
+    } else if (compChoice === "paper") {
       return "Lost";
-    } else if (computerChoice === "scissors") {
+    } else if (compChoice === "scissors") {
       return "Won";
     }
   } else if (userChoice === "paper") {
-    if (computerChoice === "rock") {
+    if (compChoice === "rock") {
       return "Won";
-    } else if (computerChoice === "paper") {
+    } else if (compChoice === "paper") {
       return "Draw";
-    } else if (computerChoice === "scissors") {
+    } else if (compChoice === "scissors") {
       return "Lost";
     }
   } else if (userChoice === "scissors") {
-    if (computerChoice === "rock") {
+    if (compChoice === "rock") {
       return "Lost";
-    } else if (computerChoice === "paper") {
+    } else if (compChoice === "paper") {
       return "Won";
-    } else if (computerChoice === "scissors") {
+    } else if (compChoice === "scissors") {
       return "Draw";
     }
   }
 };
 
-const updateScores = function (result) {
-  let userScore = 0;
-  let compScore = 0;
+const updateScores = function (result, userChoice, compChoice) {
   if (result === "Won") {
     userScore++;
+    userScoreEl.innerText = userScore;
+    resultEl.innerText = "You Won";
   } else if (result === "Lost") {
     compScore++;
+    compScoreEl.innerText = compScore;
+    resultEl.innerText = "You Lost";
   } else if (result === "Draw") {
+    resultEl.innerText = "Game Draw";
   }
+  userChoiceEl.innerText = `You choose ${userChoice.toUpperCase()}`;
+  compChoiceEl.innerText = `Comp choose ${compChoice.toUpperCase()}`;
 };
 const playGame = function (weapon) {
   const userChoice = weapon;
-  const computerChoice = getComputerChoice();
-  const result = getResult(userChoice, computerChoice);
-  getResult(userChoice, computerChoice);
-  console.log(userChoice, computerChoice, result);
+  const compChoice = getCompChoice();
+  const result = getResult(userChoice, compChoice);
+  updateScores(result, userChoice, compChoice);
 };
 
 // event listeners
